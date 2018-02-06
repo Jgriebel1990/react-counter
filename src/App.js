0eacfc03c2e1c060a5e11e40331fbbfaf97bfcce
jefferson Griebel <jefferson.griebel@gmail.com>
2018-02-06 18:56:41 -0500

    setup create-react-app project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello Moto</h1>
+        <h2>something else </h2>
+      </div>
+      
+    );
+  }
+}
+
+export default App;
