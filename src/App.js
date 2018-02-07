51601b717ace4ca70a91f43f6a747db48f05d9af
jefferson Griebel <jefferson.griebel@gmail.com>
2018-02-06 21:02:01 -0500

    created a reset button now trying to set intervals to chose from

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ class App extends Component {
   constructor (){
     super();
     this.state = {
-        count: 0
+        count: 0,
+        numberInterval: 1000
     }
     this.increment = this.increment.bind(this); //binding mean inside of "increment" the 
                             // "this" keyword is the component
     this.decrement = this.decrement.bind(this);
+
+    this.reset = this.reset.bind(this);
+
   }
   
 increment(){
@@ -17,12 +21,24 @@ increment(){
 decrement(){
   this.setState({count: this.state.count - 1});
 }
+reset(){
+  this.setState({count: this.state.reset = 0});
+}
+interval = (e) => {
+  const change = e.target.options[e.target.selectedIndex].value;
+  this.setState({numberInterval: onChange})
+}
 render(){
   return (
     <div>
       <button onClick={this.increment}>+</button>
       <button onClick={this.decrement}>-</button>
       <h1>{this.state.count}</h1>
+      <button onClick={this.reset}>Reset</button>
+      <button onChange={this.interval}>
+        interval
+        <input type="text"/>
+      </button>
     </div>
   );
 }
