b69b24aa4e55e9725cbde2e40a0ec4eb4f287640
jefferson Griebel <jefferson.griebel@gmail.com>
2018-02-06 19:48:36 -0500

    made the increments and decrements work on a counter. additionall, created the layout for a counter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor (){
+    super();
+    this.state = {
+        count: 0
+    }
+  }
   render() {
     return (
       <div>
-        <h1>Hello Moto</h1>
-        <h2>something else </h2>
+        <button onClick={() => this.setState({count: this.state.count + 1})}>+</button>
+        <button onClick={() => this.setState({count: this.state.count - 1})}>-</button>
+        <h1>{this.state.count}</h1>
       </div>
-      
     );
   }
 }
